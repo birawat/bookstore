97a29f5512fcd79986bad82b6eb1c42c2471e1c1
Vishal Prajapati <vishal.panorama08@gmail.com>
2022-10-07 19:53:27 +0530

    seller form

--- src/MainContent/Seller/index.js
+++ src/MainContent/Seller/index.js
@@ -4,14 +4,28 @@ export default function Seller() {
     
         return (
             <div className="d-flex justify-content-center mt-3">
-            <div className="border w-25 ">
+                
+            <div className="border w-50 p-3 rounded bg-light">
+            <h4 className='floatheading float-start rounded bg-light'>Sellers</h4>
                 <div className="mb-3">
                     <label for="exampleFormControlInput1" className="form-label" >Name</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Name"/>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Enter the Book Name"/>
                 </div>
                 <div className="mb-3">
                     <label for="exampleFormControlInput1" className="form-label">Price</label>
-                    <input type="Number" className="form-control" id="exampleFormControlInput1"placeholder="Price" />
+                    <input type="Number" className="form-control" id="exampleFormControlInput1"placeholder="Enter the Book Price" />
+                </div>
+                <div className="mb-3">
+                    <label for="exampleFormControlInput1" className="form-label">Category </label><br/>
+                    <select style={{width:"-webkit-fill-available",height: "40px", color:"black"}}>
+                        <option>Select Category</option>
+                        <option>New Arrivals</option>
+                        <option>Box Sets</option>
+                        <option>Best Sellers</option>
+                        <option>Fiction Books</option>
+                        <option>Award Winners</option>                        
+                    </select>
+                    
                 </div>
                 <div className="mb-3">
                     <label for="exampleFormControlInput1" className="form-label">Books Image</label>
@@ -19,9 +33,9 @@ export default function Seller() {
                 </div>
                 <div className="mb-3">
                     <label for="exampleFormControlTextarea1" className="form-label">Description</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Message"></textarea>
                 </div>
-                <div className="col-lg">
+                <div className="col-lg text-center">
                             <button type="submit" className="btn btn-primary mt-1 justify-content-center">Upload</button>
                         </div>
             </div>
