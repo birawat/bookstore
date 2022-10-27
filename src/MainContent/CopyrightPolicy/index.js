8c59667f5ef576f77c1b9eff29722f412c7e64b9
Vishal <Vishal.panorama08@gmail.com>
2022-10-27 14:00:13 +0530

    copyright page

--- src/MainContent/CopyrightPolicy/index.js
+++ src/MainContent/CopyrightPolicy/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function CopyrightPolicy() {
     return (
-        <div style={{marginLeft:"5%", marginTop:"2%"}}>
+        <div style={{ marginLeft: "5%", marginTop: "2%" }}>
             <h3>Copyright</h3>
             <p>Copyright in this Website belongs to SR Ecommerce Factory Private Limited ("BooksMania"). BooksMania also uses contents of their vendors and third parties as well who might not be the original owners of copyright therein. The users should assume that standard copyright protection applies to all materials and contents displayed on the Website. Any redistribution, modification or reproduction of part or all of the contents featured in the Website in any form is prohibited. You may display, print, or download the contents to a local hard disk extracts for your personal, non-commercial use, but only if you acknowledge the Website as the source of the material. You are not permitted, except with the express written consent of BooksMania, to distribute or commercially exploit the contents on this Website. You are also prohibited from transmitting the contents or storing it in any other Website or in other form of electronic retrieval system.</p>
             <h4>Intellectual Property Rights Policy</h4>
@@ -12,17 +12,16 @@ export default function CopyrightPolicy() {
             <p>All Marks, domain name, trade dress including the "look and feel" of the Website, its text, graphics, button icons, interfaces, etc., and the arrangements thereof, is the property of BooksMania or its vendors or the third party, as the case may be, unless otherwise indicated. All the contents are protected under copyright, trademark and other applicable intellectual property laws and may not be used by You, except as permitted by BooksMania, its vendors or the concerned third party.</p>
             <h4>Notification of copyright infringement - [Take Down Notice]</h4>
             <p>1. If you are the owner of copyright in any content available on www.BooksMania.com ("Website" or "BooksMania") and uploaded without your consent, or you believe that your content has been copied in any way that constitutes infringement, please provide the following information to the Company Secretary/ Manager Legal of BooksMania:</p>
-            
-            <ul style={{listStyleType:"circle"}}>
-            <li>A statement specifying that you have found specific content on the Website e.g., video/ audio/ text/ graphic content, which you believe infringes your copyright;</li>
-            <li>The title and description of the content, as available on the Website, and the full URL of the web page where the content is available;</li>
-            <li>Description of the content/ work, the copyright - which belongs to you, and which you believe has been infringed. In case this information is available on the internet, please send BooksMania a link to the same;</li>
-            <li>The country in which your copyright applies;</li>
-            <li>An explanation on how the content available on the Website infringes your copyright (e.g. in case of any audio content, the musical works or the sound recordings are a copy of your original works, etc.);</li>
-            <li>Your contact information so that BooksMania can get in touch with you (e-mail address is preferred);</li>
-            <li>Your contact information which BooksMania can pass on to the submitter of the concerned content, so that he/ she can get in touch with you to resolve your complaint directly (e-mail address is preferred);</li>
-            <li>A statement by you that you have a good faith belief that the disputed use of the copyrighted work is not authorized by the copyright owner (or by a third party who is legally entitled to do so on behalf of the copyright owner, such as his agent) and is not otherwise permitted by law; and</li>
-            <li>A statement by you, made under penalty of pegirjury, that the above information in your notice is accurate and that you are the copyright owner or authorized to act on the copyright owner's behalf.</li>
+            <ul style={{ listStyleType: "circle" }}>
+                <li>A statement specifying that you have found specific content on the Website e.g., video/ audio/ text/ graphic content, which you believe infringes your copyright;</li>
+                <li>The title and description of the content, as available on the Website, and the full URL of the web page where the content is available;</li>
+                <li>Description of the content/ work, the copyright - which belongs to you, and which you believe has been infringed. In case this information is available on the internet, please send BooksMania a link to the same;</li>
+                <li>The country in which your copyright applies;</li>
+                <li>An explanation on how the content available on the Website infringes your copyright (e.g. in case of any audio content, the musical works or the sound recordings are a copy of your original works, etc.);</li>
+                <li>Your contact information so that BooksMania can get in touch with you (e-mail address is preferred);</li>
+                <li>Your contact information which BooksMania can pass on to the submitter of the concerned content, so that he/ she can get in touch with you to resolve your complaint directly (e-mail address is preferred);</li>
+                <li>A statement by you that you have a good faith belief that the disputed use of the copyrighted work is not authorized by the copyright owner (or by a third party who is legally entitled to do so on behalf of the copyright owner, such as his agent) and is not otherwise permitted by law; and</li>
+                <li>A statement by you, made under penalty of pegirjury, that the above information in your notice is accurate and that you are the copyright owner or authorized to act on the copyright owner's behalf.</li>
             </ul>
             <p>2. Please affix the electronic or physical signature of the copyright owner or person authorized to act on behalf of the owner. If you are providing notice by e-mail, a scanned physical signature or a valid electronic signature will be accepted.</p>
             <p>3. Please send your copyright infringement notice to the attention of BooksMania Company Secretary/Manager-Legalat:</p>
